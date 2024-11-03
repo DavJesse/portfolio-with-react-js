db77a88380a2d4d07b6dbb0c1fa3f59c5d04ec44
davodhiambo <jesseomolo@yahoo.com>
2024-11-03 19:29:29 +0300

    feat: add animated cube to about page.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,24 @@
+import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGitAlt, faCss3, faGolang, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
 
 const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000)
+    }, [])
+
     return (
         <div className='container about-page'>
             <div className='text-zone'>
                 <h1>
                     <AnimatedLetters
+                    letterClass={letterClass}
                     strArray={['A', 'b', 'o', 'u', 't', ' ', 'm','e']}
                     indx={15}
                     />
@@ -32,6 +44,29 @@ const About = () => {
                         <li>CSS</li>
                     </ul>
                 </p>
+            </div>
+
+            <div className='stage-cube-cont'>
+                <div className='cubespinner'>
+                    <div className='face1'>
+                        <FontAwesomeIcon icon={faHtml5} color='#DD0031' />
+                    </div>
+                    <div className='face2'>
+                        <FontAwesomeIcon icon={faCss3} color='#DD0031' />
+                    </div>
+                    <div className='face3'>
+                        <FontAwesomeIcon icon={faReact} color='#DD0031' />
+                    </div>
+                    <div className='face4'>
+                        <FontAwesomeIcon icon={faGolang} color='#DD0031' />
+                    </div>
+                    <div className='face5'>
+                        <FontAwesomeIcon icon={faJsSquare} color='#DD0031' />
+                    </div>
+                    <div className='face6'>
+                        <FontAwesomeIcon icon={faGitAlt} color='#DD0031' />
+                    </div>
+                </div>
             </div>
         </div>
     )
