21f65bc958344cf85a4900c1c4e70c2165e422a9
davodhiambo <jesseomolo@yahoo.com>
2024-11-03 11:51:11 +0300

    page layout: add animation structure to home page.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,34 @@
 import LogoTitle from '../../assets/images/logo-s.png';
+import AnimatedLetters from '../AnimatedLetters/index.js';
 import { Link } from 'react-router-dom';
 import './index.scss';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['D', 'a', 'v', 'i', 'd', 'J', 'e', 's', 's', 'e']
+    const jobArray = ['s', 'o', 'f', 't', 'w', 'a', 'r', 'e', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi,<br /> I'm
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                    <br />
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
                 <img src={LogoTitle} alt="developer" />
-                David Jesse
+                <AnimatedLetters
+                letterClass={letterClass}
+                strArray={nameArray}
+                indx={15}
+                />
                 <br />
-                software developer
+                <AnimatedLetters
+                letterClass={letterClass}
+                strArray={jobArray}
+                indx={22}
+                />
                 </h1>
                 <h2>
                 Full-Stack Developer | Golang Expert | JavaScript Afficianado
