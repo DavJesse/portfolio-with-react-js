656d50a4bea53532bfc102e0cde116574176efdb
davodhiambo <jesseomolo@yahoo.com>
2024-11-14 04:58:43 +0300

    style: add style to NotFound

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import AnimatedLetters from '../AnimatedLetters';
 import "./index.scss"
 
 const NotFound = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, []);
+
     return (
         <div className='container error-page'>
-        <div className='not-found'>
-            <h1>404 - Page Not Found</h1>
+        <div className='error-content'>
+            <h1 className='error-type'>
+                <AnimatedLetters
+                letterClass={letterClass}
+                strArray={"404 - Page Not Found".split("")}
+                indx={15}
+                />                
+                </h1>
             <p>Unfortunately, the page you seek does not exist.</p>
-            <Link to="/">Go back to homepage</Link>
+            <Link to="/" className='flat-button'>Go back to homepage</Link>
         </div>
         </div>
     )
